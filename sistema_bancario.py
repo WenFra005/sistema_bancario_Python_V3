5204caddf001dd5c32c75da2eb6b4ecd153a6111
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-16 14:46:04 -0300

    UPDATE: Implementa a classe "Depósito" que extende da classe "Transacao".

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -5,6 +5,13 @@ class Transacao:
     def registrar(self, conta):
         pass
 
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    def registrar(self, conta):
+        conta.saldo += self.valor
+        conta.extrato.append(self.valor)
 
 class Banco:
     def __init__(self):
